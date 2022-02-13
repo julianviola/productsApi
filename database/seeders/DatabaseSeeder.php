0153f1a66037531f68a486ce2695be2108c93797
Julian Alihuen Viola <gotrealit@gmail.com>
2022-02-13 19:34:46 -0300

    Add Database Seeder
    Added Products Factories to DatabaseSeeder.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Product;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,16 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        Product::factory(5)->create(['name' => "MacBook Pro 13.3 Retina [MYD82] M1 Chip 256 GB - Space Gray", 
+                'description' => "",
+                'image' => "apple.com/v/macbook-pro/ac/images/overview/hero_13__d1tfa5zby7e6_large_2x.jpg", 
+                'brand' => "Apple", 
+                'price' => 2000, 
+                'price_sale' => 1950, 
+                'category' => "Macbook Pro", 
+                'stock' => 5
+            ]);
+        
+        Product::factory(50)->create();
     }
 }
