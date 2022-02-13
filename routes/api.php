7ad343e8b75491d7873505d51364f8e51e4204d6
Julian Alihuen Viola <gotrealit@gmail.com>
2022-02-13 19:46:36 -0300

    Add API Resources and Search routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProductController;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +15,15 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+/*
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});*/
+
+//Route::get('/products', [ProductController::class, 'index']);
+
+Route::prefix('v1')->group(function() {
+    Route::apiresource('/products', ProductController::class);
+    Route::get('/products/search/{name}', [ProductController::class, 'search']);
 });
+
